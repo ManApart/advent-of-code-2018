84c4e03bba8d7541156cdccc6e3c0378e370deb7
iceburg333 <ice_burg333@yahoo.com>
2018-12-03 20:51:24 -0500

    day 1 part 2 solved

--- lib/day1.ex
+++ lib/day1.ex
@@ -6,4 +6,21 @@ defmodule Day1 do
     |> Enum.map(fn x-> String.to_integer(x) end)
     |> Enum.sum
   end
+
+  def part_two(input) do
+    Regex.split(~r{[\n\r\s]+}, input)
+    |> Enum.map(fn x-> String.replace_prefix(x, "+", "") end)
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.map(fn x-> String.to_integer(x) end)
+    |> Stream.cycle
+    |> Enum.reduce_while([0], fn x, acc ->
+        if !hasCycled(x, acc), do: {:cont, [(x + List.first(acc)) | acc]}, else: {:halt, x + List.first(acc)}
+    end)
+  end
+
+  defp hasCycled(x, acc) do
+    # IO.puts(" #{x} -------  #{List.first(acc)}")
+    Enum.member?(acc, x + List.first(acc))
+  end
+
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -17,4 +17,24 @@ defmodule Day1Tests do
 
     assert Day1.part_one(input) == 8
   end
+
+  test "part two 10" do
+    input = "+3 +3 +4 -2 -4"    
+
+    assert Day1.part_two(input) == 10
+  end
+
+  test "part two 5" do
+    input = "-6 +3 +8 +5 -6"    
+
+    assert Day1.part_two(input) == 5
+  end
+
+  test "part two 14" do
+    input = "+7 +7 -2 -7 -4"    
+
+    assert Day1.part_two(input) == 14
+  end
+
+
 end
