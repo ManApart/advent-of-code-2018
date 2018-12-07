9d34f3f70b2d4c0d8c393c26a62b2ed43a12bee1
Austin Kucera <akucera@pillartechnology.com>
2018-12-07 17:31:37 -0500

    D2P2 tests pass

--- lib/day2P2.ex
+++ lib/day2P2.ex
@@ -12,41 +12,56 @@ defmodule Day2P2 do
   end
 
   defp parse(string) do
-    String.split(string, "\n")
+    string
+    |> String.trim
+    |> String.split("\n")
   end
 
   defp find_word_pair(search_word, all_words) do
     # other_words = List.delete(all_words, search_word)
-    IO.puts("find_word_pair: " <> search_word <> " " <> inspect(all_words))
-    possible_match = Enum.reduce_while(all_words, search_word, &test_word_pair/2)
-    if possible_match == search_word, do: {:cont, all_words}, else: {:halt, {search_word, possible_match}}
+    # IO.puts("find_word_pair: " <> search_word <> " " <> inspect(all_words))
+    word_search = Enum.reduce_while(all_words, search_word, &test_word_pair/2)
+    # IO.puts("word search" <> inspect(word_search))
+    case word_search do
+      {match, un_matched_indexes} -> {:halt, {match, un_matched_indexes} }
+      _ -> {:cont, all_words}
+    end
+
   end
 
   defp test_word_pair(possible_match, original_word) do
     # IO.puts("test_word_pair: " <> possible_match <> " " <> original_word)
     if possible_match == original_word, do: {:cont, original_word}
-    if !word_matches(possible_match, original_word), do: {:cont, original_word}, else: {:halt, {possible_match, original_word}}
+    un_matched_indexes = find_un_matched_indexes(possible_match, original_word)
+    # TODO - returning different arity in continue and halt
+    if length(un_matched_indexes) != 1, do: {:cont, original_word}, else: {:halt, {possible_match, un_matched_indexes}}
   end
 
-  def word_matches(word_a, word_b) do
-      {i, match_count, other_word} = Enum.reduce(String.to_char_list(word_a), {0, 0, String.to_char_list(word_b)}, &letter_matches/2)
-      IO.puts("letter count: " <> inspect(word_a) <> " " <> inspect(word_b) <> " " <> inspect(match_count))
-      (String.length(word_a)-match_count == 1)
+  def find_un_matched_indexes(word_a, word_b) do
+      {i, un_matched_indexes, other_word} = Enum.reduce(String.to_char_list(word_a), {0, [], String.to_char_list(word_b)}, &letter_matches/2)
+      # IO.puts("letter matches: " <> inspect(word_a) <> " " <> inspect(word_b) <> " " <> inspect(un_matched_indexes))
+      un_matched_indexes
   end
 
-  defp letter_matches(letter, {i, count, other_word}) do
+  defp letter_matches(letter, {i, un_matched_indexes, other_word}) do
     other_letter = Enum.at(other_word, i)
-    matches = cond do
-      letter == other_letter -> 1
-      true -> 0
+    new_un_matched_indexes = cond do
+      letter == other_letter -> un_matched_indexes
+      true -> [i | un_matched_indexes]
     end
-    # IO.puts("Letter: " <> inspect(letter) <> " " <> inspect(i) <> " " <> inspect(other_letter) <> " " <> inspect(matches))
-    {1+i, count + matches, other_word}
+    # IO.puts("Letter: " <> inspect(letter) <> " " <> inspect(i) <> " " <> inspect(other_letter) <> " " <> inspect(new_un_matched_indexes))
+    {1+i, new_un_matched_indexes, other_word}
   end
 
-  defp remove_unmatched_letter({_, {word_a, word_b}}) do
+  defp remove_unmatched_letter({original_word, un_matched_indexes}) do
     # Enum.reduce_while(String.to_char_list(word_a), String.to_char_list(word_b), &find_word_pair/2)
-    {word_a, word_b}
+    # {word_a, word_b}
+    i = List.first(un_matched_indexes)
+
+    original_word
+    |> String.to_char_list
+    |> List.delete_at(i)
+    |> List.to_string
   end
 
 end
--- test/day2P2_test.exs
+++ test/day2P2_test.exs
@@ -18,11 +18,11 @@ defmodule Day2P2Tests do
 
 
   test "part two - words match 0" do
-    assert Day2P2.word_matches("fghij", "abcde") == false
+    assert [4,3,2,1, 0] == Day2P2.find_un_matched_indexes("fghij", "abcde")
   end
 
   test "part two - words match 4" do
-    assert Day2P2.word_matches("fghij", "fguij") == true
+    assert [2] == Day2P2.find_un_matched_indexes("fghij", "fguij")
   end
 
 end
